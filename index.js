214e7bb527b850d4e5b9feed47d015e322d1bb60
Himel Nag Rana <hnrana@gmail.com>
2019-01-22 13:27:05 +0600

    showing todays pic

--- index.js
+++ index.js
@@ -1,38 +1,51 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Inert = require('inert');
+const axios = require('axios');
+const config = require('./config');
 
 const server = Hapi.server({
-    port: 3000,
+    port: 8989,
     host: 'localhost'
 });
 
 server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, h) => {
+  method: 'GET',
+  path: '/',
+  handler: (request, h) => {
+    var queryString = config.endpointBase
+                        .replace('{CLIENTID}', config.clientId)
+                        .replace('{REDIRECTURI}', config.redirectUri);
 
-        return 'Hello, world!';
-    }
+    return h.redirect(queryString);
+  }
 });
 
 server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler: (request, h) => {
+  method: 'GET',
+  path: '/receiveToken',
+  handler: (request, h) => {
+    return h.file('./token.html');
+  }
+});
 
-        return 'Hello, ' + encodeURIComponent(request.params.name) + '!';
-    }
+server.route({
+  method: 'GET',
+  path: '/showPic',
+  handler: (request, h) => {
+    //return h.file('./token.html');
+    return "pic received!";
+  }
 });
 
 const init = async () => {
-
     await server.start();
+    await server.register(Inert);
     console.log(`Server running at: ${server.info.uri}`);
 };
 
 process.on('unhandledRejection', (err) => {
-
     console.log(err);
     process.exit(1);
 });
